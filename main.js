0d162a33284acc3e07141671d9807b76c9b8891b
marcosjoseph <marquijoseph@gmail.com>
2023-07-12 19:02:14 -0300

    calculadoraPies

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 function calculadoraPies () {
+    let cantidad = prompt("Por favor ingrese la cantidad de listones");
+    let largo = prompt("Ingrese el largo en cm");
+    let ancho = prompt("Ingrese el ancho en cm");
+    let grosor = prompt("Ingrese el grosor en cm");
+    const pie = 30;
+    const pulgada = 2.54;
+    const pies = parseFloat(cantidad * (largo/pie) * (ancho/pulgada) * (grosor/pie));
+    const total = pies.toFixed(2);
 
-    let cantidad = prompt("Por favor ingrese la cantidad de listones.")
-    let largo = prompt("Ingrese el largo en cm")
-    let ancho = prompt("Ingrese el ancho en cm ")
-    let grosor = prompt("Ingrese el grosor en cm")
-    const pie = 30
-    const pulgada = 2.54
-    const pies = cantidad * (largo/pie) * (ancho/pulgada) * (grosor/pie)
-    const total = pies.toFixed(2)
-
-    return total
+    if (isNaN(total)) {
+        alert ("Por favor, ingresar valores numéricos.");}
+    
+    else { 
+        return alert (`La cantidad de pies de madera que necesitas son ${total} pies.`);}
 }
 
-let bienvenida = alert("Bienvenido al calculador de pies de madera!")
+let bienvenida = alert("Bienvenido al calculador de pies de madera!");
 
-alert ("La cantidad de pies de madera que necesitas son " + calculadoraPies() +" pies.")
+calculadoraPies();
 
 while (true) {
-    let agregar = confirm ("¿Queres calcular otra medida?")
-    if (agregar==true) { alert ("La cantidad de pies de madera que necesitas son " + calculadoraPies() +" pies.") }
-    else { alert ("Muchas gracias!") 
-    break}
+    let agregar = confirm ("¿Queres calcular otra medida?");
+    if (agregar==true) {
+        calculadoraPies();}
+    else { 
+        alert ("Muchas gracias!") 
+        break}
 }
